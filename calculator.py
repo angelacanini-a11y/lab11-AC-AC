3b4151fef01ffd7d209b0d013ed31c9d9290aeff
Angela Canini (1) <angela.canini@ufl.edu>
2025-11-21 16:20:54 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -5,6 +5,15 @@ calculator.py
 One function per operation, in order.
 """
 import math
+
+def square_root(a):
+    try:
+        if a < 0: raise ValueError("Cannot pass a negative number...")
+        return math.sqrt(a)
+    except ValueError as e:
+        print(e)
+
+def hypotenuse(a,b): return math.hypot(a,b)
 # First example
 def add(a, b): return a + b
 
@@ -19,7 +28,7 @@ def divide(a, b):
 
 def logarithm(a, b):
     if b <=0 or a == 1 or a == 0:
-        raise ZeroDivisionError("Illegal argument given...")
+        raise ValueError("Illegal argument given...")
     return math.log(b,a)# use math library/raise ValueError
 
 def exponent(a, b): return a**b
