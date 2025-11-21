0383b69d5ab9f7d7ed88e676a74f5b497e680773
Angela Canini (1) <angela.canini@ufl.edu>
2025-11-21 16:25:36 -0500

    merged changes p2

--- calculator.py
+++ calculator.py
@@ -17,29 +17,19 @@ def hypotenuse(a,b): return math.hypot(a,b)
 # First example
 def add(a, b): return a + b
 
-def sub(a, b): return a - b
 def mul(a,b): return a**b
 def div(a,b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by 0!")
     return b/a
-
-def log(a,b):
-    if b<=0 or a==1 or a==0:
-        raise ValueError("Illegal argument has been given..")
-    return math.log(b,a)
 def exp(a,b): return a**b
 
 
 def subtract(a, b): return a - b
-
-def multiply(a, b): return a * b
-
 
 def logarithm(a, b):
     if b <=0 or a == 1 or a == 0:
         raise ValueError("Illegal argument given...")
     return math.log(b,a)# use math library/raise ValueError
 
-def exponent(a, b): return a**b
 
