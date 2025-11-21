52be285192a751112ba748c3df504d64895c3a29
Angela Canini (2) <angie.canini@gmail.com>
2025-11-21 16:14:13 -0500

    modified calculator p1 lawd

--- calculator.py
+++ calculator.py
@@ -4,8 +4,20 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
-def add(a, b): 
-    pass
+def add(a, b): return a + b
+def sub(a, b): return a - b
+def mul(a,b): return a**b
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by 0!")
+    return b/a
+
+def log(a,b):
+    if b<=0 or a==1 or a==0:
+        raise ValueError("Illegal argument has been given..")
+    return math.log(b,a)
+def exp(a,b): return a**b
 
 
